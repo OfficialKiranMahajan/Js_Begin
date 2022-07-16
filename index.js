8bc30720f10577858b6607e3f4d9ecee5a0e3213
Kiran Mahajan <>
2022-07-16 06:22:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Variable Is nothing but reserved space in memory /Container");
+let a = 65;
+console.log(a);
+a = "Kiran";
+console.log(a);
+//This is an example of dyanamically typied language
